dcb251e1aaa5a36160ce121ad48c700fb7beed23
prithm <pritspido@gmail.com>
2025-04-01 08:42:46 +0000

    add pipeline code executable

---
+++ run_pipeline.sh
@@ -0,0 +1,65 @@
+python generic/explora_sc.py mistral7b_16 strategyqa full 12
+python generic/explora_sc.py mistral7b_16 gsm8k full 12
+python generic/explora_sc.py mistral7b_16 tabmwp full 12
+python generic/explora_sc.py mistral7b_16 aquarat full 12
+python generic/explora_sc.py mistral7b_16 finqa full 12
+
+cp output/aquarat_mistral7b_16_selected_exemplar.csv output/aquarat_mistral7b_32_selected_exemplar.csv
+cp output/strategyqa_mistral7b_16_selected_exemplar.csv output/strategyqa_mistral7b_32_selected_exemplar.csv
+cp output/finqa_mistral7b_16_selected_exemplar.csv output/finqa_mistral7b_32_selected_exemplar.csv
+cp output/gsm8k_mistral7b_16_selected_exemplar.csv output/gsm8k_mistral7b_32_selected_exemplar.csv
+cp output/tabmwp_mistral7b_16_selected_exemplar.csv output/tabmwp_mistral7b_32_selected_exemplar.csv
+
+cp output/aquarat_mistral7b_16_selected_exemplar.csv output/aquarat_llama3b_16_selected_exemplar.csv
+cp output/strategyqa_mistral7b_16_selected_exemplar.csv output/strategyqa_llama3b_16_selected_exemplar.csv
+cp output/finqa_mistral7b_16_selected_exemplar.csv output/finqa_llama3b_16_selected_exemplar.csv
+cp output/gsm8k_mistral7b_16_selected_exemplar.csv output/gsm8k_llama3b_16_selected_exemplar.csv
+cp output/tabmwp_mistral7b_16_selected_exemplar.csv output/tabmwp_llama3b_16_selected_exemplar.csv
+
+cp output/aquarat_mistral7b_16_selected_exemplar.csv output/aquarat_llama1b_16_selected_exemplar.csv
+cp output/strategyqa_mistral7b_16_selected_exemplar.csv output/strategyqa_llama1b_16_selected_exemplar.csv
+cp output/finqa_mistral7b_16_selected_exemplar.csv output/finqa_llama1b_16_selected_exemplar.csv
+cp output/gsm8k_mistral7b_16_selected_exemplar.csv output/gsm8k_llama1b_16_selected_exemplar.csv
+cp output/tabmwp_mistral7b_16_selected_exemplar.csv output/tabmwp_llama1b_16_selected_exemplar.csv
+
+cp output/aquarat_mistral7b_16_selected_exemplar.csv output/aquarat_llama3b_32_selected_exemplar.csv
+cp output/strategyqa_mistral7b_16_selected_exemplar.csv output/strategyqa_llama3b_32_selected_exemplar.csv
+cp output/finqa_mistral7b_16_selected_exemplar.csv output/finqa_llama3b_32_selected_exemplar.csv
+cp output/gsm8k_mistral7b_16_selected_exemplar.csv output/gsm8k_llama3b_32_selected_exemplar.csv
+cp output/tabmwp_mistral7b_16_selected_exemplar.csv output/tabmwp_llama3b_32_selected_exemplar.csv
+
+cp output/aquarat_mistral7b_16_selected_exemplar.csv output/aquarat_llama1b_32_selected_exemplar.csv
+cp output/strategyqa_mistral7b_16_selected_exemplar.csv output/strategyqa_llama1b_32_selected_exemplar.csv
+cp output/finqa_mistral7b_16_selected_exemplar.csv output/finqa_llama1b_32_selected_exemplar.csv
+cp output/gsm8k_mistral7b_16_selected_exemplar.csv output/gsm8k_llama1b_32_selected_exemplar.csv
+cp output/tabmwp_mistral7b_16_selected_exemplar.csv output/tabmwp_llama1b_32_selected_exemplar.csv
+
+python generic/explora_sc.py mistral7b_32 strategyqa test 12
+python generic/explora_sc.py mistral7b_32 gsm8k test 12
+python generic/explora_sc.py mistral7b_32 tabmwp test 12
+python generic/explora_sc.py mistral7b_32 aquarat test 12
+python generic/explora_sc.py mistral7b_32 finqa test 12
+
+python generic/explora_sc.py llama1b_16 strategyqa test 12
+python generic/explora_sc.py llama1b_16 gsm8k test 12
+python generic/explora_sc.py llama1b_16 tabmwp test 12
+python generic/explora_sc.py llama1b_16 aquarat test 12
+python generic/explora_sc.py llama1b_16 finqa test 12
+
+python generic/explora_sc.py llama1b_32 strategyqa test 12
+python generic/explora_sc.py llama1b_32 gsm8k test 12
+python generic/explora_sc.py llama1b_32 tabmwp test 12
+python generic/explora_sc.py llama1b_32 aquarat test 12
+python generic/explora_sc.py llama1b_32 finqa test 12
+
+python generic/explora_sc.py llama3b_16 strategyqa test 12
+python generic/explora_sc.py llama3b_16 gsm8k test 12
+python generic/explora_sc.py llama3b_16 tabmwp test 12
+python generic/explora_sc.py llama3b_16 aquarat test 12
+python generic/explora_sc.py llama3b_16 finqa test 12
+
+python generic/explora_sc.py llama3b_32 strategyqa test 12
+python generic/explora_sc.py llama3b_32 gsm8k test 12
+python generic/explora_sc.py llama3b_32 tabmwp test 12
+python generic/explora_sc.py llama3b_32 aquarat test 12
+python generic/explora_sc.py llama3b_32 finqa test 12
